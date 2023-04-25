66cd3fdb01f6b7c7e0e4aad81db650bb77c0dde8
Debjit Das <debjit@essbbsr.esspl.com>
2023-04-25 15:40:08 +0530

    fix

--- converter.js
+++ converter.js
@@ -2,7 +2,7 @@ const inputField = document.getElementById('input-temp');
 const fromUnitField = document.getElementById('input-unit');
 const toUnitField = document.getElementById('output-unit');
 const outputField = document.getElementById('output-temp');
-const form = document.getElementById('converter');
+const converter = document.getElementById('converter');
 
 function convertTemp(value, fromUnit, toUnit) {
   if (fromUnit === 'c') {
